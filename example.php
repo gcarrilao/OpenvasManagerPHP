7d5d8e687519da87b6d135ceb6a9573b978df703
Federico Carrilao <federico.carrilao@gba.gob.ar>
2019-07-30 11:53:42 -0300

    cambiado example

--- example.php
+++ example.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 require_once __DIR__ . '/vendor/autoload.php'; // Autoload files using Composer autoload
 
@@ -6,4 +6,21 @@ use Openvas\OpenvasManager;
 
 $ov = new OpenvasManager("localhost","9390","admin","admin");
 
-print_r($ov->get_version());
+//print_r($ov->get_version());
+
+
+$options= array(
+	"complexity" => false,
+	"host" => "10.3.8.199",
+);
+
+//print_r($ov->get_targets($options));
+
+
+$options= array(
+	"complexity" => False,
+	"report_id" => "ae2a11cd-495d-419e-94e2-2158197ec97f",
+	 "format_id" => "c1645568-627a-11e3-a660-406186ea4fc5"
+);
+
+print_r($ov->get_reports($options));
